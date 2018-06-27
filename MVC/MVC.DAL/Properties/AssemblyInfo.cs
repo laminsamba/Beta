0e067a9cc80a65053daaa4ddee82b32139d0c41c
Lamin Samba <Lamin.Samba@ofgem.gov.uk>
2018-06-27 22:04:17 +0100

    LS : Add DAL project

--- MVC/MVC.DAL/Properties/AssemblyInfo.cs
+++ MVC/MVC.DAL/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("9a2b7f06-a14d-4004-96d4-190199201015")]
+[assembly: Guid("91e898a4-ab4d-4ced-ba40-59b096863b5c")]
 
 // Version information for an assembly consists of the following four values:
 //
