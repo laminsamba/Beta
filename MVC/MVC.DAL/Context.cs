d511828699b8b56bf55d8b6a2b8800a14bf05ea2
Lamin Samba <Lamin.Samba@ofgem.gov.uk>
2018-06-27 17:05:40 +0100

    LS : Add new project

---
+++ MVC/MVC.DAL/Context.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity.EntityFramework;
+using WebApp.MVC.Models;
+
+namespace MVC.DAL
+{
+    public class Context : IdentityDbContext<ApplicationUser>
+    {
+        public DbSet<Team> Teams { get; set; }  
+    }
+
+    public class Team
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
